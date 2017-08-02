96c121750fc6674557ed8be6c6c495734125592c
Jasna Blemberg <jasnab@rainn.org>
2017-08-02 01:30:04 -0400

    Add Anagram puzzle

--- src/main/java/puzzle/Anagram.java
+++ src/main/java/puzzle/Anagram.java
@@ -1,4 +1,44 @@
 package puzzle;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Anagram {
+
+    public static boolean isAnagram(String word, String testWord) {
+        if (word == null || testWord == null || word.trim().length() == 0 || testWord.trim().length() == 0)
+            return false;
+
+        word = formatStringForComparison(word);
+        testWord = formatStringForComparison(testWord);
+        if (word.trim().length() != testWord.trim().length())
+            return false;
+
+        Map<Character, Integer> wordMap = new HashMap<>();
+        for (int startTest = 0; startTest < testWord.length(); startTest++) {
+            if (wordMap.containsKey(testWord.charAt(startTest))) {
+                int count = wordMap.get(testWord.charAt(startTest)) + 1;
+                wordMap.put(testWord.charAt(startTest), count);
+            }
+            wordMap.put(testWord.charAt(startTest), 1);
+        }
+        for (int start = 0; start < word.length(); start++) {
+            if (wordMap.containsKey(word.charAt(start))) {
+                int count = wordMap.get(testWord.charAt(start)) - 1;
+                wordMap.put(testWord.charAt(start), count);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : wordMap.entrySet()) {
+            if (entry.getValue() > 0)
+                return false;
+        }
+        return true;
+    }
+
+    private static String formatStringForComparison(String string) {
+        if (string == null)
+            return null;
+        return string.replaceAll(" ", "").replaceAll("[^0-9a-zA-Z]+", "").trim().toLowerCase();
+    }
 }
--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.Test;
+import puzzle.Anagram;
+import puzzle.Palindrome;
 
 public class Tests {
+
+    @Test
+    public void testPalindrome() {
+        Assert.assertTrue(Palindrome.isPalindrome("Ee"));
+        // Case insensitivity
+        Assert.assertTrue(Palindrome.isPalindrome("Deleveled"));
+        // Many words
+        Assert.assertTrue(Palindrome.isPalindrome("Animal loots foliated detail of stool lamina"));
+        // Wrong
+        Assert.assertFalse(Palindrome.isPalindrome("Hello world!"));
+    }
+
+    @Test
+    public void testAnagram() {
+        Assert.assertTrue(Anagram.isAnagram(" Mother-in-law", "Woman Hitler"));
+        Assert.assertTrue(Anagram.isAnagram("These churn air", "The Hurricanes"));
+        Assert.assertTrue(Anagram.isAnagram("Conversation", "Voices rant on"));
+        Assert.assertFalse(Anagram.isAnagram("dflkj", "ioj-09sdf"));
+        Assert.assertFalse(Anagram.isAnagram("hello there", "dolly faces"));
+        Assert.assertFalse(Anagram.isAnagram("wtf", "wtd"));
+        Assert.assertFalse(Anagram.isAnagram("wtf", "wtdyooo"));
+        Assert.assertFalse(Anagram.isAnagram("wtfooo", "wtd"));
+    }
 }
